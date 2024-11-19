c29fac341b2825e3ad2739f6d76c35a5409d1bc3
Fred Lai <fred.lai@futureverse.com>
2024-11-20 10:20:29 +1300

    feat: Enroll audit account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "audit" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "cnapp-audit@futureverse.com"
+    AccountName               = "Audit"
+    ManagedOrganizationalUnit = "Security (ou-1r4w-l60z7t65)"
+    SSOUserEmail              = "fred.lai@futureverse.com"
+    SSOUserFirstName          = "Fred"
+    SSOUserLastName           = "Lai"
+  }
+
+  account_tags = {
+    "Enroll" = "audit"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Fred"
+    change_reason       = "Enroll audit account to Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
